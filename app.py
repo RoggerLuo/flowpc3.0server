5cfbae97fedbd076c835bcc4f7492aaa8132dd0d
Roger <luojie.5408@163.com>
2018-12-11 18:39:46 +0800

    Update

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from category import *
-from note import *
-from notes import *
+#from category import query_categories,create_category,modify_category,delete_category
+#from note import create_note,modify_note,delete_note
+from note import create_note#,modify_note,delete_note
+
+from notes import query_notes
 from flask import Flask, request
 import flask_cors
 app = Flask(__name__, static_folder='static')
@@ -13,27 +15,30 @@ def notes(): # 各种条件查询
     pageNum = request.args.get('pageNum')
     return query_notes(categoryId,pageSize,pageNum)
 
-@app.route('/note/<note_id>', methods=['POST', 'PUT', 'DELETE'])
-def note(note_id): # CRUD
-    if request.method == 'POST':
-        return create_note() # return note_id
-    if request.method == 'PUT':
-        return modify_note(note_id)
-    if request.method == 'DELETE':
-        return delete_note(note_id)
+@app.route('/note', methods=['POST']) # post方法要使用  x-www-form来访问，不然400
+def notedd(): 
+    content = request.form['content']
+    return create_note(content) # return note_id
 
-@app.route('/categories', methods=['GET'])
-def categories(): # 各种条件查询
-    return query_categories()
+# @app.route('/note/<note_id>', methods=['POST', 'DELETE'])
+# def note_with_id(note_id):
+#     if request.method == 'PUT':
+#         return modify_note(note_id)
+#     if request.method == 'DELETE':
+#         return delete_note(note_id)
 
-@app.route('/category/<category_id>', methods=['POST', 'PUT', 'DELETE'])
-def category(note_id): # CRUD
-    if request.method == 'POST':
-        return create_category() # return category_id
-    if request.method == 'PUT':
-        return modify_category(category_id)
-    if request.method == 'DELETE':
-        return delete_category(category_id)
+# @app.route('/categories', methods=['GET'])
+# def categories(): # 各种条件查询
+#     return query_categories()
+
+# @app.route('/category/<category_id>', methods=['POST', 'PUT', 'DELETE'])
+# def category(note_id): # CRUD
+#     if request.method == 'POST':
+#         return create_category() # return category_id
+#     if request.method == 'PUT':
+#         return modify_category(category_id)
+#     if request.method == 'DELETE':
+#         return delete_category(category_id)
 
 
 if __name__ == '__main__':
--- category.py
+++ category.py
@@ -1,51 +1,18 @@
-import mysql.connector
-import jieba
+from _mysql import run
 import numpy as np
-
-
-def connect2Mysql():
-    conn = mysql.connector.connect(
-        user='root', password='as56210', database='flow4.0', use_unicode=True)
-    cursor = conn.cursor()
-    return conn, cursor
-
-
-def getIgnores():
-    conn, cursor = connect2Mysql()
-    cursor.execute('SELECT * from ignore_list')
-    values = cursor.fetchall()
-    cursor.close()
-    conn.close()
-    return [v[1] for v in values]
-
-
-def get_high_frequency_list():
-    conn, cursor = connect2Mysql()
-    cursor.execute('SELECT * from high_frequency_list')
-    values = cursor.fetchall()
-    cursor.close()
-    conn.close()
-    return [v[1] for v in values]
-
-
-class String2word(object):
-
-    def __init__(self):
-        self.jieba = jieba
-        self.word_list = []
-        self.ignored_list = getIgnores()
-        self.high_frequency_list = get_high_frequency_list()
-
-    def segment(self,string):
-        self.word_list = self.jieba.lcut(string)  # 默认是精确模式
-        return self
-
-    def filter(self):
-        filtered_list = []
-        for word in self.word_list:
-            if word not in self.ignored_list:
-                if word not in self.high_frequency_list:
-                    filtered_list.append(word)
-
-        self.word_list = filtered_list
-        return filtered_list
+def query_categories(categoryId,pageSize,pageNum):
+    if pageNum == None:
+        pageNum = 1
+    if pageSize == None:
+        pageSize = 10
+    if categoryId == None:
+        categoryId = False
+    pageNum = int(pageNum)
+    pageSize = int(pageSize)
+    def callback(conn,cursor):
+        cursor.execute(
+            "SELECT * from note where status = 0 Order By modify_time Desc limit %s,%s",
+            [(pageNum-1)*pageSize,pageSize]
+        )
+        return cursor.fetchall()
+    return run(callback)
