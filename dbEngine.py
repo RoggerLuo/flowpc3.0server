3885163d5e727236f5e24c25227509546db1a0ce
rogerFromUbuntu <luojie.5408@163.com>
2018-12-18 03:22:15 +0000

    mod

--- dbEngine.py
+++ dbEngine.py
@@ -2,7 +2,7 @@ import mysql.connector
 import json
 
 def run(callback):
-    conn = mysql.connector.connect(port='6033',user='root', password='!@as56210', database='flowpc3.0', use_unicode=True)
+    conn = mysql.connector.connect(host='172.17.0.1',port='6033',user='root', password='!@as56210', database='flowpc3.0', use_unicode=True)
     cursor = conn.cursor()
     try:
         values = callback(conn, cursor)
