81d7de073643272e9453a1a1f6a7fffa2d97ff4c
Roger <luojie.5408@163.com>
2019-01-04 00:59:31 +0800

    upda

--- dao/classify.py
+++ dao/classify.py
@@ -4,7 +4,7 @@ import time
 def classify(note_id,cate_id):
     def callback(conn,cursor):
         now = time.time()
-        cursor.execute('UPDATE note set category = %s, create_time = %s where id = %s', [cate_id,now,note_id])
+        cursor.execute('UPDATE note set category = %s, create_time = %s, training_mark=0 where id = %s', [cate_id,now,note_id])
         conn.commit()
         return 'success'
     return run(callback)
