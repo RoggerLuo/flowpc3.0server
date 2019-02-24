837e611ce08c60ea776c851b65c6e39bef229dc1
Roger <luojie.5408@163.com>
2019-02-24 11:34:28 +0800

    update from mac pro

--- app.py
+++ app.py
@@ -4,13 +4,16 @@ import jieba
 import json
 app = Flask(__name__, static_folder='static')
 flask_cors.CORS(app, supports_credentials=True)
+from dao.ignoreList import get_ignore_list
 
 @app.route('/wordscut', methods=['POST'])
 def wordscut(): 
     if request.method == 'POST':
         content = request.form['content']
         words_list = jieba.lcut_for_search(content)
-    return json.dumps(words_list,ensure_ascii=False)
+        ignore_list = get_ignore_list()
+        note_word_list = list(filter(lambda x:x not in ignore_list,words_list))
+    return json.dumps(note_word_list,ensure_ascii=False)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=6664, debug=False) 
